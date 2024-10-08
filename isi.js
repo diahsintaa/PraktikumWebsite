151c75968b28c325d2733bf2c5c3475e4cd7d39f
diahsintaa <diahsintanr@gmail.com>
2024-10-08 14:52:01 +0700

    isi.js

---
+++ isi.js
@@ -0,0 +1,7 @@
+document.writeln("<center>");
+document.writeln("<hr width=600 color=Black>");
+document.writeln("<h1>UNIVERSITAS LAMPUNG</H1>");
+document.writeln("<h2>PENDIDIKAN TEKNOLOGI INFORMASI</H2>");
+document.writeln("<h3>JURUSAN PENDIDIKAN TEKNOLOGI INFORMASI</H3>");
+document.writeln("<hr width=600 color=Black>");
+document.writeln("</center>");
